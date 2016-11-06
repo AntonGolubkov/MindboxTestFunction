3b5921cf3020393a808f3a893e27ca2499ca08a2
AntonTheFiremind <AntonTheFiremind@ATF-PC>
2016-11-06 17:02:25 +0300

    Add project files.

---
+++ MindboxTestFunction/TravelCard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MindboxTestFunction
+{
+    public class TravelCard
+    {
+        public string Source { get; set; }
+        public string Destination { get; set; }
+
+        // Требуемая заданием функция.
+        // Временная сложность алгоритма сортировки: в лучшем случае - O(n+1); в среднем случае - O(2n^2); в худшем случае - O(2n^2).
+        // Ёмкостная сложность алгоритма сортировки: O(n).
+        public static List<TravelCard> SortCards(List<TravelCard> cards)
+        {
+            List<TravelCard> sortedCards = new List<TravelCard>();
+
+            // Поиск первой карточки.
+            foreach (TravelCard card in cards)
+            {
+                // Если пункт отправления карточки не повторяется в пунктах назначения других карточек, то такая карточка будет первой.
+                if (cards.Find(c => c.Destination == card.Source) == null)
+                {
+                    sortedCards.Add(card);
+
+                    // Добавление остальных карточек в упорядоченный список.
+                    for (int i = 0; i < cards.Count; i++)
+                    {
+                        // Если пункт отправления карточки совпадает с пунктом назначения последней карточки в упорядоченном списке, то такая карточка добавляется в список.
+                        if (cards[i].Source == sortedCards.Last().Destination)
+                        {
+                            sortedCards.Add(cards[i]);
+                            i = -1;
+                            continue;
+                        }
+                    }
+
+                    break;
+                }
+            }
+
+            return sortedCards;
+        }
+    }
+}
---
+++ MindboxTestFunctionUnitTests/TravelCardUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MindboxTestFunction
+{
+    [TestClass]
+    public class TravelCardUnitTest
+    {
+        [TestMethod]
+        public void TestSortCards()
+        {
+            // Arrange
+            List<TravelCard> cards = new List<TravelCard>();
+            cards.Add(new TravelCard() { Source = "Берлин", Destination = "Лиссабон" });
+            cards.Add(new TravelCard() { Source = "Милан", Destination = "Мюнхен" });
+            cards.Add(new TravelCard() { Source = "Лиссабон", Destination = "Москва" });
+            cards.Add(new TravelCard() { Source = "Мюнхен", Destination = "Берлин" });
+            cards.Add(new TravelCard() { Source = "Рим", Destination = "Милан" });
+
+            // Act
+            List<TravelCard> sortedCards = TravelCard.SortCards(cards);
+
+            // Assert
+            Assert.AreEqual("Рим", sortedCards.First().Source);
+            Assert.AreEqual("Москва", sortedCards.Last().Destination);
+            Assert.AreEqual(cards.Count, sortedCards.Count);
+        }
+    }
+}
